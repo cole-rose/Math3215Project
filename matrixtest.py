e783317fcd189958392a673dbcf0082e8ed37b43
cole-rose <coletherose@gmail.com>
2018-04-07 23:28:54 -0400

    first commit

---
+++ matrixtest.py
@@ -0,0 +1,9 @@
+import numpy as np
+from pandas import *
+import pandas as pd
+# I = pd.Index(["a", "b", "c"], name="rows")
+# C = pd.Index(["col0", "col1", "col2"], name="columns")
+# a = [[1,2,3],[4,5,6],[7,8,9]]
+# f = open("matrixtest.txt",'w')
+
+# print(DataFrame(a,index = I, columns = C), file = f)
